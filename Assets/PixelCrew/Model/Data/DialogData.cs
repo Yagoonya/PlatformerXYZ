433a1094d8c3ee1a473f70930cb52ca1e434814e
Yagoonya <50630906+Yagoonya@users.noreply.github.com>
2022-10-22 20:15:32 +0700

    Improved dialogue system

--- Assets/PixelCrew/Model/Data/DialogData.cs
+++ Assets/PixelCrew/Model/Data/DialogData.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace PixelCrew.Model.Data
 {
     [Serializable]
     public class DialogData
     {
-        [SerializeField] private string[] _sentences;
-        public string[] Sentences => _sentences;
+        [SerializeField] private SentencesData[] _sentences;
+        public SentencesData[] Sentences => _sentences;
+
+
+        public void XYZ()
+        {
+        }
+    }
+
+
+    [Serializable]
+    public class SentencesData
+    {
+        [SerializeField] private string _sentence;
+        [SerializeField] private Sprite _avatar;
+        [SerializeField] private bool _isLeftWindow;
+
+        public string Sentence => _sentence;
+        public Sprite Avatar => _avatar;
+        public bool IsLeftWindow => _isLeftWindow;
     }
 }
--- Assets/PixelCrew/UI/Hud/Dialogs/DialogBoxController.cs
+++ Assets/PixelCrew/UI/Hud/Dialogs/DialogBoxController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using PixelCrew.Model.Data;
 using PixelCrew.Utils;
 using UnityEngine;
@@ -9,16 +8,21 @@ namespace PixelCrew.UI.Hud.Dialogs
 {
     public class DialogBoxController : MonoBehaviour
     {
+        [Header("Left")] [SerializeField] private GameObject _container;
         [SerializeField] private Text _text;
-        [SerializeField] private GameObject _container;
-        [SerializeField] private Animator _animator;
+        [SerializeField] private Image _avatar;
+
+        [Header("Right")] [SerializeField] private GameObject _containerR;
+        [SerializeField] private Text _textR;
+        [SerializeField] private Image _avatarR;
 
         [Space] [SerializeField] private float _textSpeed = 0.09f;
 
         [Header("Sounds")] [SerializeField] private AudioClip _typing;
+        [SerializeField] private Animator _animator;
         [SerializeField] private AudioClip _open;
         [SerializeField] private AudioClip _close;
-        
+
         private static readonly int IsOpen = Animator.StringToHash("IsOpen");
 
         private DialogData _data;
@@ -36,20 +40,30 @@ namespace PixelCrew.UI.Hud.Dialogs
             _data = data;
             _currentSentence = 0;
             _text.text = string.Empty;
-            
-            _container.SetActive(true);
+
+            if (_data.Sentences[_currentSentence].IsLeftWindow)
+            {
+                _container.SetActive(true);
+                _avatar.sprite =_data.Sentences[_currentSentence].Avatar;
+            }
+            else
+            {
+                _containerR.SetActive(true);
+                _avatarR.sprite =_data.Sentences[_currentSentence].Avatar;
+            }
             _sfxSource.PlayOneShot(_open);
             _animator.SetBool(IsOpen, true);
         }
-        
-        private IEnumerator TypeDialogText()
+
+        private IEnumerator TypeDialogText(Text textContainer, GameObject container)
         {
-            _text.text = string.Empty;
-            var sentence = _data.Sentences[_currentSentence];
-            
+            container.SetActive(true);
+            textContainer.text = string.Empty;
+            var sentence = _data.Sentences[_currentSentence].Sentence;
+
             foreach (var letter in sentence)
             {
-                _text.text += letter;
+                textContainer.text += letter;
                 _sfxSource.PlayOneShot(_typing);
                 yield return new WaitForSeconds(_textSpeed);
             }
@@ -59,10 +73,10 @@ namespace PixelCrew.UI.Hud.Dialogs
 
         public void OnSkip()
         {
-            if(_typingRoutine == null) return;
+            if (_typingRoutine == null) return;
 
             StopTypeAnimation();
-            _text.text = _data.Sentences[_currentSentence];
+            _text.text = _data.Sentences[_currentSentence].Sentence;
         }
 
         public void OnContinue()
@@ -78,38 +92,45 @@ namespace PixelCrew.UI.Hud.Dialogs
 
                 var isDialogCompleted = _currentSentence >= _data.Sentences.Length;
                 if (isDialogCompleted)
-                {
                     HideDialogBox();
-                }
                 else
-                {
                     OnStartDialogAnimation();
-                }
             }
         }
 
         private void HideDialogBox()
         {
-           _animator.SetBool(IsOpen, false);
-           _sfxSource.PlayOneShot(_close);
+            _animator.SetBool(IsOpen, false);
+            _sfxSource.PlayOneShot(_close);
         }
 
         private void StopTypeAnimation()
         {
-            if(_typingRoutine != null)
+            if (_typingRoutine != null)
                 StopCoroutine(_typingRoutine);
             _typingRoutine = null;
         }
 
         private void OnStartDialogAnimation()
         {
-            _typingRoutine = StartCoroutine(TypeDialogText());
+            if (_data.Sentences[_currentSentence].IsLeftWindow)
+            {
+                _avatar.sprite = _data.Sentences[_currentSentence].Avatar;
+                _typingRoutine = StartCoroutine(TypeDialogText(_text, _container));
+                _containerR.SetActive(false);
+            }
+            else
+            {
+                _avatarR.sprite = _data.Sentences[_currentSentence].Avatar;
+                _typingRoutine = StartCoroutine(TypeDialogText(_textR, _containerR));
+                _container.SetActive(false);
+            }
         }
 
         private void OnCloseDialogAnimation()
         {
-
-
+            _container.SetActive(false);
+            _containerR.SetActive(false);
         }
     }
 }
